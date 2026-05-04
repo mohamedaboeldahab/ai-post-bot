6a0d24cbc16048be85eb08bb9e8c7e4469bd31dd
mohamedaboeldahab <108568318+mohamedaboeldahab@users.noreply.github.com>
2026-05-04 09:48:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,11 +11,27 @@ const db = admin.firestore();
 
 // شخصيات AI
 const characters = [
-  "أحمد المستثمر",
-  "ليلى الريادية",
-  "تامر المغامر"
+  {
+    name: "أحمد حسن",
+    photo: "https://i.pravatar.cc/150?img=12",
+    style: "هادئ وبيتكلم عن الاستثمار طويل المدى"
+  },
+  {
+    name: "سارة محمد",
+    photo: "https://i.pravatar.cc/150?img=5",
+    style: "تحفيزية وبتحب ريادة الأعمال"
+  },
+  {
+    name: "كريم علي",
+    photo: "https://i.pravatar.cc/150?img=8",
+    style: "عملي وبيتكلم عن التجارة والمشاريع"
+  },
+  {
+    name: "منى خالد",
+    photo: "https://i.pravatar.cc/150?img=20",
+    style: "بتدي نصائح مالية بسيطة"
+  }
 ];
-
 // بوستات جاهزة (ممكن تطورها AI بعدين)
 const posts = [
   "الفرص مش بتستنى حد 🚀",
